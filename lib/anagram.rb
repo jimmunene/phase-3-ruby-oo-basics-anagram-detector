b3ed161e83d0494a3eba54aea30f3dcb82bcef3a
jimbrian <ui.ux.jimbrian@gmail.com>
2022-10-20 16:15:54 +0300

    anagram detector

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(other_words)
+        matches = []
+        for word in other_words do
+            if word.split("").sort == @word.split("").sort
+                matches.push(word)
+            end
+        end
+        matches
+    end
+end
